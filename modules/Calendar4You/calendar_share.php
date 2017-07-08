c9299b3b05f9bdca36e5ee949b8071daa0fd4729
Brandon Garcia <bcgarcia44@gmail.com>
2017-07-07 19:14:06 -0500

    chore() fixed 'short open tag' warning by expanding it to <?php

--- modules/Calendar4You/calendar_share.php
+++ modules/Calendar4You/calendar_share.php
@@ -204,7 +204,7 @@ $Calendar_Settings = $Calendar4You->getSettings();
 			<td align="left"><b>
 			<?php echo $mod_strings["LBL_GOOGLE_SYNC_ACCESS_DATA"]; echo " &quot;".trim($current_user->first_name." ".$current_user->last_name)."&quot;"; ?>
 			</b>
-			<br /><br /><div id="google_sync_verifying" style="display:none;"><img src="themes/images/vtbusy.gif" align="absmiddle"><? echo $mod_strings["LBL_GOOGLE_SYNC_CONTROL_ACCESS_DATA"]; ?></div><div id="google_sync_text">
+			<br /><br /><div id="google_sync_verifying" style="display:none;"><img src="themes/images/vtbusy.gif" align="absmiddle"><?php echo $mod_strings["LBL_GOOGLE_SYNC_CONTROL_ACCESS_DATA"]; ?></div><div id="google_sync_text">
 			<?php
 			$GoogleSync4You = new GoogleSync4You();
 			$have_access_data = $GoogleSync4You->setAccessDataForUser($current_user->id);
